fbb2928f0505bfb7dde9db84d81e3a78c3c5ab46
irtizahussain223 <irtizahussain223@gmail.com>
2021-10-09 07:17:22 -0700

    Node back end server added to serve pizza items using mongoose and MongoDB.

---
+++ client_side/src/components/navbar/navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "./navbar.css";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" className="Navbar">
+        <Navbar.Brand href="#home">SHEY PIZZA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Login</Nav.Link>
+            <Nav.Link href="#link">Cart</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+};
+
+export default NavBar;
