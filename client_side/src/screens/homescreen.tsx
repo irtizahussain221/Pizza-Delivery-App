49312d262a903e89f129f73bbe737c806b0c0c91
irtizahussain223 <irtizahussain223@gmail.com>
2021-10-24 19:08:44 +0500

    Filter functionality added

--- client_side/src/screens/homescreen.tsx
+++ client_side/src/screens/homescreen.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPizzas } from "./../redux/actions/pizzaAction";
 import Pizza from "../components/pizza/pizza";
@@ -11,6 +11,8 @@ function Homescreen() {
     (state: RootState) => state.allPizzas.pizzas
   );
   const dispatch = useDispatch();
+  const [categoryPizzas, setCategoryPizzas] = useState([] as Pizzas[]);
+  const [category, selectedCategory] = useState("all");
 
   function getPizzas() {
     axios
@@ -25,16 +27,57 @@ function Homescreen() {
     getPizzas();
   }, []);
 
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    selectedCategory(e.currentTarget.value);
+    if (e.currentTarget.value === "veg" || "nonveg") {
+      setCategoryPizzas(
+        pizzas.filter((pizza) => {
+          return pizza.category === e.currentTarget.value;
+        })
+      );
+    }
+  };
+
+  if (!pizzas[0]) {
+    return <>...Loading</>;
+  }
+
   return (
-    <div className="row">
-      {pizzas.map((pizza, key) => {
-        return (
-          <div className="col-md-4 row-eq-height" key={key}>
-            <Pizza pizza={pizza} />
+    <>
+      <div className="container">
+        <div className="row mt-5 bg-white rounded shadow-lg p-3 mb-5 justify-content-center">
+          <div className="col-md-4 m-3">
+            <select
+              className="form-select"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            >
+              <option value="all">All</option>
+              <option value="veg">Veg</option>
+              <option value="nonveg">Non Veg</option>
+            </select>
           </div>
-        );
-      })}
-    </div>
+        </div>
+      </div>
+      <div className="row">
+        {category === "all"
+          ? pizzas.map((pizza, key) => {
+              return (
+                <div className="col-md-4 row-eq-height" key={key}>
+                  <Pizza pizza={pizza} />
+                </div>
+              );
+            })
+          : categoryPizzas.map((pizza, key) => {
+              return (
+                <div className="col-md-4 row-eq-height" key={key}>
+                  <Pizza pizza={pizza} />
+                </div>
+              );
+            })}
+      </div>
+    </>
   );
 }
 
